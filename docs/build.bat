57f319e7db8a0b8dd7b5d4c58e1e46011099e7a5
Baumeister <dbaumeis@amd.com>
2017-04-21 15:03:23 +0200

    Removed full python path.

--- docs/build.bat
+++ docs/build.bat
@@ -1,5 +1,5 @@
 C:/Python36/python -m venv env
 call env/scripts/activate.bat
-C:/Python36/python -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip -q install -r requirements.txt
-C:/Python36/python docgen.py
+python docgen.py

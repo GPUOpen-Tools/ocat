93eec6ce1e48c38f35e642e6c95cf097c2adcb04
Baumeister <dbaumeis@amd.com>
2017-04-21 14:45:03 +0200

    Added appveyor file and changed build bat for docs to work with appveyor.

--- docs/build.bat
+++ docs/build.bat
@@ -1,5 +1,5 @@
-python -m venv env
+C:/Python36/python -m venv env
 call env/scripts/activate.bat
-python -m pip install --upgrade pip
+C:/Python36/python -m pip install --upgrade pip
 pip -q install -r requirements.txt
-python docgen.py
+C:/Python36/python docgen.py

ce4a03521db59fb220a240ec8889cf72adc69bc6
Baumeister <dbaumeis@amd.com>
2017-04-25 14:38:17 +0200

    Added pre-build script.

---
+++ pre-build.ps1
@@ -0,0 +1,34 @@
+Write-Host "Get git submodules"
+git submodule update --init --recursive
+Write-Host "Get git submodules [DONE]"
+
+if(!(Test-Path .\OCAT-Installer\redist)) 
+{ 
+    mkdir .\OCAT-Installer\redist 
+}
+if(!(Test-Path .\OCAT-Installer\redist\vc_redist.x64.exe)) 
+{ 
+    Write-Host "Download vc_redist.x64.exe"
+    Invoke-WebRequest -Uri https://download.microsoft.com/download/5/7/b/57b2947c-7221-4f33-b35e-2fc78cb10df4/vc_redist.x64.exe -OutFile .\OCAT-Installer\redist\vc_redist.x64.exe
+    Write-Host "Download vc_redist.x64.exe [DONE]"
+}
+else {
+    Write-Host "vc_redist.x64.exe already up to date."
+}
+
+if(!(Test-Path .\OCAT-Installer\redist\vc_redist.x86.exe)) 
+{ 
+    Write-Host "Download vc_redist.x86.exe"
+    Invoke-WebRequest -Uri https://download.microsoft.com/download/1/d/8/1d8137db-b5bb-4925-8c5d-927424a2e4de/vc_redist.x86.exe -OutFile .\OCAT-Installer\redist\vc_redist.x86.exe
+    Write-Host "Download vc_redist.x86.exe [DONE]"
+}
+else {
+    Write-Host "vc_redist.x86.exe already up to date."
+}
+
+Write-Host "Build documentation"
+cd docs
+.\build.bat
+cd ..
+Write-Host "Build documentation [DONE]"
+

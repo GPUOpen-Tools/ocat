3a9af0e838287601bc0dc3f718ee98454ba9a6c2
Lou Kramer <lou.kramer@amd.com>
2018-01-31 16:51:46 +0100

    Remove x86 configurations in installer.

--- build.ps1
+++ build.ps1
@@ -7,14 +7,6 @@ msbuild OCAT.sln /t:Build /p:Configuration=Release /p:Platform=x86 /verbosity:mi
 Write-Host "Build OCAT x86 [DONE]"
 
 Write-Host "Build Installer"
-msbuild OCAT.sln /t:Installer /p:Configuration=Release /p:Platform=x86 /verbosity:minimal
-Write-Host "Build Installer [DONE]"
-
-Write-Host "Build Install Bundle"
-msbuild OCAT.sln /t:Install-Bundle /p:Configuration=Release /p:Platform=x86 /verbosity:minimal
-Write-Host "Build Install Bundle [DONE]"
-
-Write-Host "Build Installer"
 msbuild OCAT.sln /t:Installer /p:Configuration=Release /p:Platform=x64 /verbosity:minimal
 Write-Host "Build Installer [DONE]"
 

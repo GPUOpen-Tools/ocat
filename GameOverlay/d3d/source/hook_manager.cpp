921f60b1a1533d75d3da0841eb3e5123a7637f27
aurolou <lou.kramer@amd.com>
2019-07-17 17:20:22 +0200

    Fix dx overlay (#80)
    Fix overlay compatibility issue with some dx games.

--- GameOverlay/d3d/source/hook_manager.cpp
+++ GameOverlay/d3d/source/hook_manager.cpp
@@ -812,6 +812,39 @@ namespace GameOverlay {
     int numModulesRegistered = 0;
     g_messageLog.LogInfo("register_module", L"Register module for " + target_path);
 
+    if (!install_hook(reinterpret_cast<hook::address>(&::LoadLibraryA),
+                      reinterpret_cast<hook::address>(&HookLoadLibraryA))) {
+      g_messageLog.LogError("register_module", "Failed to install hook for LoadLibraryA");
+    }
+    else {
+      g_messageLog.LogInfo("register_module", "Successfully installed hook for LoadLibraryA");
+      numModulesRegistered++;
+    }
+    if (!install_hook(reinterpret_cast<hook::address>(&::LoadLibraryExA),
+                      reinterpret_cast<hook::address>(&HookLoadLibraryExA))) {
+      g_messageLog.LogError("register_module", "Failed to install hook for LoadLibraryExA");
+    }
+    else {
+      g_messageLog.LogInfo("register_module", "Successfully installed hook for LoadLibraryExA");
+      numModulesRegistered++;
+    }
+    if (!install_hook(reinterpret_cast<hook::address>(&::LoadLibraryW),
+                      reinterpret_cast<hook::address>(&HookLoadLibraryW))) {
+      g_messageLog.LogError("register_module", "Failed to install hook for LoadLibraryW");
+    }
+    else {
+      g_messageLog.LogInfo("register_module", "Successfully installed hook for LoadLibraryW");
+      numModulesRegistered++;
+    }
+    if (!install_hook(reinterpret_cast<hook::address>(&::LoadLibraryExW),
+                      reinterpret_cast<hook::address>(&HookLoadLibraryExW))) {
+      g_messageLog.LogError("register_module", "Failed to install hook for LoadLibraryExW");
+    }
+    else {
+      g_messageLog.LogInfo("register_module", "Successfully installed hook for LoadLibraryExW");
+      numModulesRegistered++;
+    }
+
     HMODULE handle = nullptr;
     GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_PIN, target_path.c_str(), &handle);
 
@@ -884,4 +917,13 @@ namespace GameOverlay {
 
     return hook.call();
   }
-}
+
+  __declspec(dllexport) void remove_libraryA_hooks()
+  {
+    hook hookExA = find_hook(reinterpret_cast<hook::address>(&HookLoadLibraryExA));
+    hook hookA = find_hook(reinterpret_cast<hook::address>(&HookLoadLibraryA));
+
+    uninstall_hook(hookExA, hook_method::function_hook);
+    uninstall_hook(hookA, hook_method::function_hook);
+  }
+  }
